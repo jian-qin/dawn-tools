21b2f229b3391eaf078aa11e489a069bd453d677
剑倾 <1904741371@qq.com>
2024-09-14 19:58:01 +0800

    ✨ feat: 新增功能：粘贴光标所在标签的属性；优化功能：格式化选中html代码片段、复制/删除光标所在标签的属性、封装的工具函数

--- src/html.ts
+++ src/html.ts
@@ -1,56 +1,53 @@
 import { commands, window, Selection, env } from 'vscode'
-import { getNowHtmlTagRange, getIndentationMode, getHtmlAst } from './tools'
+import { waitSelectionChange, getNowHtmlTagRange, getIndentationMode, isTagWrap, getHtmlAst, getNearHtmlAttr } from './tools'
 
 // 格式化选中html代码片段（自动选中光标所在html）
 commands.registerCommand("dawn-tools.html.format", async () => {
-  const newRange = getNowHtmlTagRange()
-  if (!newRange) return
+  const tagRange = getNowHtmlTagRange()
+  if (!tagRange) return
   const editor = window.activeTextEditor!
-  const tag = editor.document.getText(newRange)
+  const tag = editor.document.getText(tagRange)
   const ast = getHtmlAst(tag)
   let newTag = ast.openStart._content
-  const end = ast.openEnd._content
+  const end = ast.openEnd.content
   const tab = getIndentationMode().tab
-  // 判断是否需要换行
-  const firstAttr = ast.attributes.find((attr: any) => attr.key.content.trim())
-  const firstAttrLine = editor.document.positionAt(editor.document.offsetAt(newRange.start) + firstAttr.key.startPosition + 1).line
   // 格式化标签
-  if (firstAttrLine === newRange.start.line) {
-    const line = editor.document.lineAt(newRange.start.line)
+  if (isTagWrap(tagRange, ast)) {
+    if (ast._attributes) {
+      const attrStr = ast._attributes.map((attr: any) => attr._assembly.replaceAll(`\n${tab}`, '\n')).join(' ')
+      newTag += ` ${attrStr}`
+    }
+    newTag += `${ast.openEnd._isClose ? ' ' : ''}${end}`
+  } else {
+    const line = editor.document.lineAt(tagRange.start.line)
     const baseTab = line.text.substring(0, line.firstNonWhitespaceCharacterIndex)
     const attrTab = `\n${baseTab}${tab}`
-    const attrStr = ast._attributes.map((attr: any) => attr._assembly.replaceAll('\n', `\n${tab}`)).join(attrTab)
-    newTag += `${attrTab}${attrStr}\n${baseTab}${end}`
-  } else {
-    const attrStr = ast._attributes.map((attr: any) => attr._assembly.replaceAll(`\n${tab}`, '\n')).join(' ')
-    newTag += ` ${attrStr}${end[0] === '/' ? ' ' : ''}${end}`
+    if (ast._attributes) {
+      const attrStr = ast._attributes.map((attr: any) => attr._assembly.replaceAll('\n', `\n${tab}`)).join(attrTab)
+      newTag += `${attrTab}${attrStr}`
+    }
+    newTag += `\n${baseTab}${end}`
   }
   // 设置新的标签
-  await editor.edit(editBuilder => {
-    editBuilder.replace(newRange, newTag)
-  })
-  // 选中新的标签
-  editor.selection = new Selection(newRange.start, getNowHtmlTagRange()!.end)
+  await editor.edit(editBuilder => editBuilder.replace(tagRange, newTag))
+  // 选中更新的标签
+  editor.selection = new Selection(tagRange.start, getNowHtmlTagRange()!.end)
+  await waitSelectionChange()
 })
 
 // 复制光标所在标签的属性
 commands.registerCommand("dawn-tools.html.attr.copy", async () => {
-  const newRange = getNowHtmlTagRange()
-  if (!newRange) return
+  const tagRange = getNowHtmlTagRange()
+  if (!tagRange) return false
   const editor = window.activeTextEditor!
-  const tag = editor.document.getText(newRange)
-  const ast = getHtmlAst(tag)
-  const startIndex = editor.document.offsetAt(editor.selection.start) - editor.document.offsetAt(newRange.start)
-  const offsets: number[] = ast._attributes.map((attr: any) => Math.min(
-    Math.abs(startIndex - attr.key.startPosition),
-    Math.abs(startIndex - attr._endPosition),
-  ))
-  const attr = ast._attributes[offsets.indexOf(Math.min(...offsets))]
-  const beforeGap = tag.substring(0, attr.key.startPosition).match(/\s+$/)![0]
+  const tag = editor.document.getText(tagRange)
+  const attr = getNearHtmlAttr(tagRange)?.attr
+  if (!attr) return false
+  const beforeGap = tag.substring(0, attr.key.startPosition).match(/\s+$/)?.[0] || ''
   env.clipboard.writeText(beforeGap + attr._assembly)
   editor.selection = new Selection(
-    editor.document.positionAt(editor.document.offsetAt(newRange.start) + attr.key.startPosition - beforeGap.length),
-    editor.document.positionAt(editor.document.offsetAt(newRange.start) + attr._endPosition + 1)
+    editor.document.positionAt(editor.document.offsetAt(tagRange.start) + attr.key.startPosition - beforeGap.length),
+    editor.document.positionAt(editor.document.offsetAt(tagRange.start) + attr._endPositionTrim + 1)
   )
   return true
 })
@@ -58,7 +55,64 @@ commands.registerCommand("dawn-tools.html.attr.copy", async () => {
 // 删除光标所在标签的属性
 commands.registerCommand("dawn-tools.html.attr.delete", async () => {
   const isCopy = await commands.executeCommand('dawn-tools.html.attr.copy')
-  if (!isCopy) return
+  if (!isCopy) return false
   await commands.executeCommand('deleteLeft')
   return true
+})
+
+// 粘贴光标所在标签的属性
+commands.registerCommand("dawn-tools.html.attr.paste", async () => {
+  let text = (await env.clipboard.readText()).trim()
+  if (!text) return false
+  const tagRange = getNowHtmlTagRange()
+  if (!tagRange) return false
+  const editor = window.activeTextEditor!
+  const tag = editor.document.getText(tagRange)
+  const ast = getHtmlAst(tag)
+  const tab = getIndentationMode().tab
+  const getBeforeGap = (position: number) => tag.substring(0, position).match(/\s+$/)?.[0] || ''
+  let nodePosition: number
+  // 是否有属性
+  if (ast._attributes) {
+    const { startIndex, attr } = getNearHtmlAttr(tagRange, ast)!
+    const beforeGap = getBeforeGap(attr.key.startPosition)
+    // 光标更靠近属性名的开始/结束
+    if (Math.abs(startIndex - attr.key.startPosition) < Math.abs(startIndex - attr._endPositionTrim)) {
+      nodePosition = attr.key.startPosition
+      if (isTagWrap(tagRange, ast)) {
+        text = `${text}${beforeGap}`
+      } else {
+        text = `${text} `
+      }
+    } else {
+      nodePosition = attr._endPositionTrim + 1
+      if (isTagWrap(tagRange, ast)) {
+        text = `${beforeGap}${text}`
+      } else {
+        if (ast.openEnd._isClose) {
+          text = tag[attr._endPositionTrim + 1] === ' ' ? ` ${text}` : ` ${text} `
+        } else {
+          text = ` ${text}`
+        }
+      }
+    }
+  } else {
+    nodePosition = ast.openEnd.startPosition
+    if (isTagWrap(tagRange, ast)) {
+      const beforeGap = getBeforeGap(nodePosition)
+      text = `${tab}${text}${beforeGap}`
+    } else {
+      if (ast.openEnd._isClose) {
+        text = tag[nodePosition - 1] === ' ' ? `${text} ` : ` ${text} `
+      } else {
+        text = ` ${text}`
+      }
+    }
+  }
+  const newTag = `${tag.substring(0, nodePosition)}${text}${tag.substring(nodePosition)}`
+  await editor.edit(editBuilder => editBuilder.replace(tagRange, newTag))
+  editor.selection = new Selection(
+    editor.document.positionAt(editor.document.offsetAt(tagRange.start) + nodePosition),
+    editor.document.positionAt(editor.document.offsetAt(tagRange.start) + nodePosition + text.length)
+  )
 })
--- src/tools.ts
+++ src/tools.ts
@@ -65,26 +65,14 @@ export function getHtmlEndIndex(beforePosition: Position) {
     if (!match) return
     const tag = text.substring(0, match.index + 1)
     // 是否合法的 html 标签
-    const ast = parser(tag)[0]
-    if (ast) {
-      // 开始标签匹配失败
-      if (!isLegalHtmlTag(ast)) return null
-      afterIndex = beforeIndex + match.index + 1
-      break
-    }
+    const ast: any = parser(tag)
+    // 开始标签匹配失败
+    if (!ast[0]?.tag) return null
+    afterIndex = beforeIndex + match.index + 1
+    break
   } while (reg.lastIndex < text.length)
   if (!afterIndex) return
   return document.positionAt(afterIndex)
-}
-
-// 是否合法的 html 标签
-function isLegalHtmlTag(ast: any) {
-  const keys = Object.keys(ast.attrs || {})
-  // 无属性标签，直接中断操作
-  if (!keys.length) return
-  // 是否合法的attribute和tag
-  const noLegalAttribute = [ast.tag, ...keys].some(key => ['"', "'"].includes(key.at(-1)))
-  return !noLegalAttribute
 }
 
 // 获取光标所在的标签位置
@@ -112,19 +100,58 @@ export function getNowHtmlTagRange() {
 // 获取html标签的ast语法树
 export function getHtmlAst(tag: string) {
   const ast: any = constructTree(tokenize(tag).tokens).ast.content.children[0].content
-  ast.openStart._content = ast.openStart.content.trim()
-  ast.openEnd._content = ast.openEnd.content.trim()
-  ast._attributes = ast.attributes.flatMap((attr: any) => {
-    let _assembly = attr.key.content
-    if (attr.value) {
-      _assembly += `=${attr.startWrapper?.content || ''}${attr.value.content}${attr.endWrapper?.content || ''}`
+  ast.openStart._content = ast.openStart.content.trimEnd()
+  ast.openEnd._isClose = ast.openEnd.content[0] === '/'
+  if (ast.attributes) {
+    ast._attributes = ast.attributes.flatMap((attr: any) => {
+      let _assembly = attr.key.content
+      if (attr.value) {
+        _assembly += `=${attr.startWrapper?.content || ''}${attr.value.content}${attr.endWrapper?.content || ''}`
+      }
+      const len = _assembly.length
+      _assembly = _assembly.trimEnd()
+      const offset = len - _assembly.length
+      const _endPosition = (attr.endWrapper || attr.value || attr.key).endPosition
+      const _endPositionTrim = _endPosition - offset
+      return _assembly ? [{
+        ...attr,
+        _assembly,
+        _endPosition,
+        _endPositionTrim,
+      }] : []
+    })
+    if (ast._attributes.length === 0) {
+      delete ast._attributes
     }
-    _assembly = _assembly.trim()
-    return _assembly ? [{
-      ...attr,
-      _assembly,
-      _endPosition: (attr.endWrapper || attr.value || attr.key).endPosition,
-    }] : []
-  })
+  }
   return ast
+}
+
+// 判断标签是否换行
+export function isTagWrap(
+  tagRange = getNowHtmlTagRange()!,
+  ast: any = getHtmlAst(window.activeTextEditor!.document.getText(tagRange))
+) {
+  const editor = window.activeTextEditor!
+  const firstNodePosition = ast._attributes ? ast._attributes[0].key.startPosition : ast.openEnd.startPosition
+  const firstNodeLine = editor.document.positionAt(editor.document.offsetAt(tagRange.start) + firstNodePosition + 1).line
+  return firstNodeLine !== tagRange.start.line
+}
+
+// 获取光标最近的标签属性
+export function getNearHtmlAttr(
+  tagRange = getNowHtmlTagRange()!,
+  ast: any = getHtmlAst(window.activeTextEditor!.document.getText(tagRange))
+) {
+  if (!ast._attributes) return
+  const editor = window.activeTextEditor!
+  const startIndex = editor.document.offsetAt(editor.selection.start) - editor.document.offsetAt(tagRange.start)
+  const offsets: number[] = ast._attributes.map((attr: any) => Math.min(
+    Math.abs(startIndex - attr.key.startPosition),
+    Math.abs(startIndex - attr._endPositionTrim),
+  ))
+  return {
+    startIndex,
+    attr: ast._attributes[offsets.indexOf(Math.min(...offsets))],
+  }
 }
