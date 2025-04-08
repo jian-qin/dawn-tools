3a7f0ceeaa8f0abdfde6158e3644d46258a88609
剑倾 <1904741371@qq.com>
2025-04-08 18:01:17 +0800

    🐞 fix: 插入console.log

--- src/snippets.ts
+++ src/snippets.ts
@@ -1,5 +1,5 @@
 import { commands, window, env, Selection } from 'vscode'
-import { getIndentationMode, getLineIndent, positionOffset } from './tools'
+import { getNearMatchs, getIndentationMode, getLineIndent, positionOffset } from './tools'
 import { selectionsHistory } from './store'
 
 // 插入console.log(选中的字符 || 光标所在位置的单词 || 剪贴板的内容)
@@ -7,32 +7,53 @@ commands.registerCommand('dawn-tools.snippets.log', async () => {
   const editor = window.activeTextEditor
   if (!editor?.selection) return
   // 剪贴板的内容
-  await commands.executeCommand('editor.action.addSelectionToNextFindMatch')
-  const inserts = editor.selections.filter(({ isEmpty }) => isEmpty)
-  if (inserts.length) {
-    const text = (await env.clipboard.readText()).trim()
-    await editor.edit((editBuilder) => {
-      inserts.forEach(({ active }) => editBuilder.insert(active, `console.log(${text})`))
+  {
+    const matchs = getNearMatchs(/\w+/g)
+    if (!matchs) return
+    const indexs: number[] = []
+    const inserts = editor.selections.filter(({ isEmpty, active }, index) => {
+      const has = isEmpty && matchs.every(({ range }) => !range.contains(active))
+      has || indexs.push(index)
+      return has
     })
-    if (inserts.length === editor.selections.length) {
-      editor.selections = inserts.map(
-        ({ active }) => new Selection(positionOffset(active, 12), positionOffset(active, 12 + text.length))
-      )
-      return
+    if (inserts.length) {
+      const text = (await env.clipboard.readText()).trim()
+      await editor.edit((editBuilder) => {
+        inserts.forEach(({ active }) => editBuilder.insert(active, `console.log(${text})`))
+      })
+      if (inserts.length === editor.selections.length) {
+        editor.selections = inserts.map(
+          ({ active }) => new Selection(positionOffset(active, 12), positionOffset(active, 12 + text.length))
+        )
+        return
+      }
+      editor.selections = editor.selections.filter((_, index) => indexs.includes(index))
     }
   }
-  editor.selections = editor.selections.filter(({ isEmpty }) => !isEmpty)
   // 选中的字符 || 光标所在位置的单词
-  const texts = editor.selections.map((selection) => {
-    const text = editor.document.getText(selection)
-    const quote = text.includes("'") ? (text.includes('"') ? '`' : '"') : "'"
-    return `console.log(${quote}${text}${quote}, ${text})`
-  })
+  const texts = snippets_log__getSelectionAndWhereTexts()
+  if (!texts.length) return
   await commands.executeCommand('editor.action.insertLineAfter')
   await editor.edit((editBuilder) => {
     editor.selections.forEach(({ active }, index) => editBuilder.insert(active, texts[index]))
   })
 })
+function snippets_log__getSelectionAndWhereTexts() {
+  const editor = window.activeTextEditor!
+  const matchs = getNearMatchs(/\w+/g)
+  if (!matchs) {
+    return []
+  }
+  return editor.selections.map((selection) => {
+    const range = selection.isEmpty ? matchs.find(({ range }) => range.contains(selection.active))?.range : selection
+    if (!range) {
+      return ''
+    }
+    const text = editor.document.getText(range)
+    const quote = text.includes("'") ? (text.includes('"') ? '`' : '"') : "'"
+    return `console.log(${quote}${text}${quote}, ${text})`
+  })
+}
 
 // 插入console.log - 使用上一次光标位置的单词
 commands.registerCommand('dawn-tools.snippets.log.before', async () => {
@@ -42,16 +63,9 @@ commands.registerCommand('dawn-tools.snippets.log.before', async () => {
   if (!before) return
   const current = selectionsHistory.at(-1)!
   editor.selections = before
-  await commands.executeCommand('editor.action.addSelectionToNextFindMatch')
-  const texts = editor.selections.flatMap((selection) => {
-    if (selection.isEmpty) {
-      return []
-    }
-    const text = editor.document.getText(selection)
-    const quote = text.includes("'") ? (text.includes('"') ? '`' : '"') : "'"
-    return [`console.log(${quote}${text}${quote}, ${text})`]
-  })
+  const texts = snippets_log__getSelectionAndWhereTexts().filter(Boolean)
   editor.selections = current
+  if (!texts.length) return
   if (!editor.document.lineAt(editor.selection.active.line).isEmptyOrWhitespace) {
     await commands.executeCommand('editor.action.insertLineAfter')
   }
